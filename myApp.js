d5555327adef268f71f1648019d073fe37d889d1
Rishabh Panesar <rishabhpanesar@gmail.com>
2022-09-06 03:25:40 +0530

    50% express course done

--- myApp.js
+++ myApp.js
@@ -1,39 +1,23 @@
-let express = require('express');
+const env = require("dotenv").config();
+let express = require("express");
 let app = express();
+// console.log("Hello World");
+app.use("/public", express.static(`${__dirname}/public`));
 
+app.get("/", (req, res) => {
+	// res.send("Hello Express");
+	res.sendFile(`${__dirname}/views/index.html`);
+});
 
+app.get("/json", (req, res) => {
+	let str = "Hello json";
+	console.log(process.env.MESSAGE_STYLE);
+	if (process.env.MESSAGE_STYLE === "uppercase") {
+		str = str.toUpperCase();
+	}
+	res.json({
+		message: str,
+	});
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
